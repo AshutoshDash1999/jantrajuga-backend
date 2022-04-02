6b3d9350c889c0e0b6fe2982c7fc52cca61a9588
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-04-03 03:26:14 +0530

    search result ui update

--- client/src/components/searchResult/SearchResult.js
+++ client/src/components/searchResult/SearchResult.js
@@ -1,14 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const SearchResult = () => {
-    const [result, setResult] = useState([]);
-    return (<>
-        <div className="bg-gray-200 my-2 mx-4 p-3">
-            <h2>Product Name</h2>
-            <h3>Price</h3>
-            <p>Vendor name</p>
-        </div>
-    </>)
-}
+  const [result, setResult] = useState([]);
+  return (
+    <>
+      <div className="w-3/4 flex justify-center flex-col items-center content-center m-auto">
+        {result.map((item) => {
+          return (
+            <div className="bg-gray-200 my-2 mx-4 p-3 rounded-md w-1/2">
+              <h2>{item.name}</h2>
+              <h3>{item.price}</h3>
+              <p>Vendor name, 2km away from you</p>
+              <p>Delivery expected time: 2hour</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default SearchResult;
