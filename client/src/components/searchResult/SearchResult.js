a33b507eaa44af8cc23677914c3d4437e55cdc08
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-04-03 03:20:05 +0530

    search result update

--- client/src/components/searchResult/SearchResult.js
+++ client/src/components/searchResult/SearchResult.js
@@ -3,7 +3,11 @@ import React, {useState} from "react";
 const SearchResult = () => {
     const [result, setResult] = useState([]);
     return (<>
-        
+        <div className="bg-gray-200 my-2 mx-4 p-3">
+            <h2>Product Name</h2>
+            <h3>Price</h3>
+            <p>Vendor name</p>
+        </div>
     </>)
 }
 
