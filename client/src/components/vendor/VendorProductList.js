c212884fad73c8b1117a32fd90221f4d148f41f4
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-04-03 06:36:19 +0530

    add vendor product list

---
+++ client/src/components/vendor/VendorProductList.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const VendorProductList = () => {
+    const [productList, setProductList] = useState([]);
+    return(<>
+        <h1>Vendor Name</h1>
+        <h4>Vendor Id</h4>
+        {productList.map(() => {
+            return (<div>
+                <div className="bg-gray-200 my-2 mx-4 p-3 rounded-md w-1/3 h-auto">
+              <Grid container spacing={2}>
+                <Grid item xs={5}>
+                  <img className="rounded-md h-auto" src={item.photo}/>
+                </Grid>
+                <Grid item xs={7} className="whitespace-normal">
+                  <a className="font-semibold text-xl cursor-pointer">{item.name}</a>
+                  <h3>₹{item.price}</h3>
+                </Grid>
+              </Grid>
+            </div>
+            </div>)
+        })}
+    </>)
+}
+
+export default VendorProductList;
